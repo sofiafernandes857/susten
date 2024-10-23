5990580bdc170c8c454b8d2a8f48f4a45f382ef5
Sofia Fernandes <162995867+sofiafernandes857@users.noreply.github.com>
2024-10-23 19:20:44 -0300

    completando

--- src/componentes/Carros3D/index.js
+++ src/componentes/Carros3D/index.js
@@ -1,5 +1,5 @@
 import React from 'react'; 
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as NavLink, Routes, Route } from 'react-router-dom';
 import Footer from '../Footer';
 import Header from '../Header';
 import ExplorarCarro from './ExplorarCarro';
